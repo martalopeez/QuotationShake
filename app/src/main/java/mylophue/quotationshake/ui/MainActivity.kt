1788f869f587a1b6361a9ddc4145a527b2eef9eb
martalopeez <martalopeez88@gmail.com>
2023-02-20 16:01:37 +0100

    Creando proyecto

--- app/src/main/java/mylophue/quotationshake/ui/MainActivity.kt
+++ app/src/main/java/mylophue/quotationshake/ui/MainActivity.kt
@@ -3,10 +3,12 @@ package mylophue.quotationshake.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import mylophue.quotationshake.R
+import mylophue.quotationshake.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
