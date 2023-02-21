b77fefa000b737ff91a90fabfa953f904a2fed27
martalopeez <martalopeez88@gmail.com>
2023-02-21 16:30:54 +0100

    menu provider

--- app/src/main/java/mylophue/quotationshake/ui/MainActivity.kt
+++ app/src/main/java/mylophue/quotationshake/ui/MainActivity.kt
@@ -2,16 +2,44 @@ package mylophue.quotationshake.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.core.view.MenuProvider
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationBarView
+import mylophue.quotationshake.R
 import mylophue.quotationshake.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MenuProvider {
+    lateinit var binding: ActivityMainBinding
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val navController = binding.fragmentContainerView.getFragment<NavHostFragment>().navController
-        binding.bottomNavigationView.setupWithNavController(navController)
+        navController = binding.fragmentContainerView.getFragment<NavHostFragment>().navController
+        (binding.bottomNavigationView as NavigationBarView).setupWithNavController(navController)
+
+        setSupportActionBar(binding.materialToolbar)
+
+        val appBar = AppBarConfiguration(setOf(R.id.newQuotationFragment, R.id.favouritesFragment, R.id.settingsFragment))
+        setupActionBarWithNavController(navController, appBar)
+
+        addMenuProvider(this)
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.menu_about, menu)
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        navController.navigate(R.id.aboutDialogFragment)
+        return true
     }
 }
